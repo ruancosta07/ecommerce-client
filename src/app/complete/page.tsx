9226fd7976408a038144233f6d5ce207ace965e9
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-22 11:56:09 -0300

    rota que mostra os produtos comprados

---
+++ src/app/complete/page.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Success from './_components/Success'
+import DayJs from '@/utils/Dayjs';
+import { redirect } from 'next/navigation';
+import Progress from '../_components/Progress';
+interface Order {
+  total:number;
+  products: Array<{id:string;name:string;images:string;price:number; }>;
+  created:number
+  expiresAt:number;
+}
+
+
+export const metadata = {
+  title: "Compra concluída"
+}
+
+const Complete = async({searchParams}: {searchParams: Promise<{session_id: string}>}) => {
+  const {session_id} = await searchParams
+  const order = await fetch(`${process.env.NEXT_PUBLIC_API_PROD_URL}/orders/successfull/${session_id}`)
+  const result = await order.json() as Order
+  const timestampToDate = result.expiresAt * 1000
+  const expiresDate = DayJs(timestampToDate).subtract(23, "hour")
+  
+  if(DayJs().isAfter(expiresDate)){
+    redirect("/")
+  }
+  return (
+    <>
+      <Success order={result}/>
+      {/* <Progress/> */}
+    </>
+  )
+}
+
+export default Complete
