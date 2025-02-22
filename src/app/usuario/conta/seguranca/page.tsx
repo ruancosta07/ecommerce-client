01a6c4279c068775bcbead5b237745d72501d414
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-22 12:15:13 -0300

    página de segurança

---
+++ src/app/usuario/conta/seguranca/page.tsx
@@ -0,0 +1,16 @@
+import Header from '@/app/_components/Header'
+import React from 'react'
+import Container from '../_components/Container'
+
+const Conta = () => {
+  return (
+    <>
+      <Header/>
+      <Container>
+        s
+      </Container>
+    </>
+  )
+}
+
+export default Conta
