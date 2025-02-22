601f803121fa16faf6cf6d4f7776e07469e958dd
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-22 12:15:31 -0300

    rota dos favoritos

---
+++ src/app/usuario/favoritos/page.tsx
@@ -0,0 +1,15 @@
+import Header from '@/app/_components/Header'
+import React from 'react'
+import Favorites from './_components/Favorites'
+
+const page = () => {
+    
+    return (
+        <>
+            <Header />
+            <Favorites />
+        </>
+    )
+}
+
+export default page
