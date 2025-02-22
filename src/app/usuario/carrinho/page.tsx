5e4777006422ce250ed13a162ea5353743fb4c16
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-22 12:12:18 -0300

    rota do carrinho

---
+++ src/app/usuario/carrinho/page.tsx
@@ -0,0 +1,15 @@
+import Header from '@/app/_components/Header'
+import React from 'react'
+import Cart from './_components/Cart'
+
+const page = () => {
+
+    return (
+        <>
+            <Header />
+            <Cart />
+        </>
+    );
+}
+
+export default page;
