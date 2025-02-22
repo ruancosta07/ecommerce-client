e683db32f6839e964cc94e13c19f6c48706db608
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-22 11:52:52 -0300

    footer

---
+++ src/app/_components/Footer.tsx
@@ -0,0 +1,97 @@
+import { Facebook, Instagram, ShoppingBasket, Twitter } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='dark:bg-zinc-800/70 '>
+            <div className="container-width flex max-lg:gap-[2rem] flex-wrap justify-between pt-[6rem]">
+                <div className='max-md:w-full'>
+                    <Link href={"/"} className='text-[3rem] font-semibold font-space flex items-center gap-[1rem] '>
+                        <ShoppingBasket className='size-[2.6rem]' />
+                        Ecommerce
+                    </Link>
+                    <ul className='flex items-center gap-[1rem] mt-[4rem]'>
+                        <li>
+                            <a href="">
+                                <Facebook />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <Twitter />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <Instagram />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className='max-md:w-full'>
+                    <span className='text-[2.4rem] font-semibold dark:text-zinc-300/70 leading-none mb-[.8rem] block'>
+                        Moda
+                    </span>
+                    <ul className='text-[1.6rem]'>
+                        <li >
+                            <Link href={""} className=' font-medium py-[.5rem] block'>Camisas Esportivas</Link>
+                        </li>
+                        <li >
+                            <Link href={""} className=' font-medium py-[.5rem] block'>Tênis Femininos</Link>
+                        </li>
+                        <li >
+                            <Link href={""} className=' font-medium py-[.5rem] block'>Calças de Alfaiataria</Link>
+                        </li>
+                        <li >
+                            <Link href={""} className=' font-medium py-[.5rem] block'>Vestidos Floridos</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className='max-md:w-full'>
+                    <span className='text-[2.4rem] font-semibold dark:text-zinc-300/70 leading-none mb-[.8rem] block'>
+                        Usuário
+                    </span>
+                    <ul className='text-[1.6rem]'>
+                        <li >
+                            <Link href={""} className=' font-medium py-[.5rem] block'>Minha conta</Link>
+                        </li>
+                        <li >
+                            <Link href={""} className=' font-medium py-[.5rem] block'>Meus favoritos</Link>
+                        </li>
+                        <li >
+                            <Link href={""} className=' font-medium py-[.5rem] block'>Minhas compras</Link>
+                        </li>
+                        <li >
+                            <Link href={""} className=' font-medium py-[.5rem] block'>Alterar foto</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <span className='text-[2.4rem] font-semibold dark:text-zinc-300/70 leading-none mb-[.8rem] block'>
+                        Guia e ajuda
+                    </span>
+                    <ul className='text-[1.6rem]'>
+                        <li >
+                            <Link href={""} className=' font-medium py-[.5rem] block'>Minha conta</Link>
+                        </li>
+                        <li >
+                            <Link href={""} className=' font-medium py-[.5rem] block'>Meus favoritos</Link>
+                        </li>
+                        <li >
+                            <Link href={""} className=' font-medium py-[.5rem] block'>Minhas compras</Link>
+                        </li>
+                        <li >
+                            <Link href={""} className=' font-medium py-[.5rem] block'>Alterar foto</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+                <div className='w-full mt-[4rem]  p-[1rem] text-[1.6rem] '>
+                    <p className='font-space max-w-[70%] mx-auto'>&copy; 2024 - 2025. Ecommerce</p>
+                </div>
+        </footer>
+    )
+}
+
+export default Footer
