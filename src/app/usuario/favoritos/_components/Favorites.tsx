927b3fda1ce1d1d49f80fc68ff45a8fb42bee587
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-24 15:38:24 -0300

    feat: exclusão de itens do carrinho

--- src/app/usuario/favoritos/_components/Favorites.tsx
+++ src/app/usuario/favoritos/_components/Favorites.tsx
@@ -6,7 +6,7 @@ import React, { useMemo } from 'react'
 import Cookies from 'js-cookie'
 import Message, { useMessage } from '@/app/_components/Message'
 const Favorites = () => {
-    const { favorites, setFavorites } = useUser()
+    const { favorites, setFavorites, setCart } = useUser()
     const {message, setMessage} = useMessage()
     const [totalItens, totalValue, discount] = useMemo(() => {
         return [
@@ -15,6 +15,24 @@ const Favorites = () => {
             favorites.reduce((a, b) => a +(b.price * 0.1) * b.quantity, 0),
         ]
     }, [favorites])
+
+    async function moveItemToFavorites(id:string){
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_PROD_URL}/users/favorites/move-item`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${Cookies.get("authTokenUser")}`
+                },
+                body: JSON.stringify({id})
+            })
+            const data = await response.json()
+            setFavorites(data.favorites)
+            setCart(data.cart)
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
     return (
         <main>
@@ -26,7 +44,7 @@ const Favorites = () => {
                         <div>
                             <h1 className='text-[2.4rem]'>{c.name}</h1>
                             <span className='text-[1.8rem] font-medium dark:text-zinc-300'>R$ {c.price.toFixed(2)}</span>
-                                <button className='dark:bg-zinc-100 dark:text-zinc-900 text-[1.4rem] font-medium p-[1rem] block mt-[.8rem] rounded-[.6rem]'>Mover para o carrinho</button>
+                                <button onClick={()=> moveItemToFavorites(c.id)} className='dark:bg-zinc-100 dark:text-zinc-900 text-[1.4rem] font-medium p-[1rem] block mt-[.8rem] rounded-[.6rem]'>Mover para o carrinho</button>
                         </div>
                         <button className='mb-auto ml-auto text-red-400'>
                             <Trash2 />
