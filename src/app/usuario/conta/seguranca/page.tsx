98d65af40b584edfbc4f3ebcbd2ed405e49ab126
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-24 15:39:17 -0300

    refactor: lógica da rota de segurança

--- src/app/usuario/conta/seguranca/page.tsx
+++ src/app/usuario/conta/seguranca/page.tsx
@@ -1,13 +1,14 @@
 import Header from '@/app/_components/Header'
 import React from 'react'
 import Container from '../_components/Container'
+import Security from './_components/Security'
 
 const Conta = () => {
   return (
     <>
       <Header/>
       <Container>
-        s
+        <Security/>
       </Container>
     </>
   )
