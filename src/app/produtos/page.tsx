52852bd4deba2effe2b9d1fde29fa14389811a06
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-25 13:55:01 -0300

    feat: página de pesquisa de produtos

---
+++ src/app/produtos/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../_components/Header'
+import { Product } from '@/types/Product'
+import SearchProducts from './_components/Search'
+
+
+
+const page = async ({ searchParams }: { searchParams: Promise<{ produto: string; pagina: number; limite: string; max: number; min: number; order: "desc" | "asc" }> }) => {
+  const { produto, pagina, limite, max, min, order } = await searchParams
+  const result = await fetch(`${process.env.NEXT_PUBLIC_API_DEV_URL}/products/search?produto=${produto}&pagina=${pagina}&limite=${limite}&min=${min}&max=${max}&order=${order}`)
+
+  const data = await result.json() as { products: Product[]; total: number; totalPages: number; clearProducts: Product[] }
+  return (
+    <>
+      <Header />
+      <SearchProducts products={data.products} clearProducts={data.clearProducts} total={data.total} totalPages={data.totalPages} />
+
+    </>
+  )
+}
+
+export default page
