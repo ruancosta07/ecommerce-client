ab7354cb3e8ec3e46f59c57f281b2d787abf707a
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-25 14:31:32 -0300

    refactor: cores do texto

--- src/app/usuario/favoritos/_components/Favorites.tsx
+++ src/app/usuario/favoritos/_components/Favorites.tsx
@@ -2,19 +2,12 @@
 import { useUser } from '@/store/User'
 import { Trash2 } from 'lucide-react'
 import Image from 'next/image'
-import React, { useMemo } from 'react'
+import React from 'react'
 import Cookies from 'js-cookie'
 import Message, { useMessage } from '@/app/_components/Message'
 const Favorites = () => {
     const { favorites, setFavorites, setCart } = useUser()
     const {message, setMessage} = useMessage()
-    const [totalItens, totalValue, discount] = useMemo(() => {
-        return [
-            favorites.reduce((a, b) => a + b.quantity, 0),
-            favorites.reduce((a, b) => a + (b.price - (b.price * 0.1)) * b.quantity, 0),
-            favorites.reduce((a, b) => a +(b.price * 0.1) * b.quantity, 0),
-        ]
-    }, [favorites])
 
     async function moveItemToFavorites(id:string){
         try {
@@ -36,35 +29,20 @@ const Favorites = () => {
 
     return (
         <main>
-            <div className="max-w-[70%] mx-auto grid grid-cols-[.7fr_.3fr] gap-x-[4rem]">
+            <div className="container-width o grid lg:grid-cols-[.7fr_.3fr] gap-x-[4rem]">
                 <h1 className='text-[3rem] font-semibold mb-[1.2rem] col-span-full'>Meus favoritos</h1>
                 <div className='grid gap-[2rem]'>
-                    {favorites.map((c) => <div key={c.id} className='flex gap-[1rem]'>
-                        <Image src={c.images[0]} alt={c.name} width={180} height={180} className=' object-cover size-[20rem] rounded-[2rem]' />
-                        <div>
-                            <h1 className='text-[2.4rem]'>{c.name}</h1>
+                    {favorites.map((c) => <div key={c.id} className='flex flex-wrap gap-[1rem] relative'>
+                        <Image src={c.images[0]} alt={c.name} width={180} height={180} className=' object-cover max-md:size-[10rem] lg:size-[20rem] rounded-[2rem]' />
+                        <div className='flex-1'>
+                        <h1 className='text-[1.6rem] lg:text-[2.4rem] font-semibold break-all text-wrap break-words text-zinc-900 dark:text-zinc-100'>{c.name}</h1>
                             <span className='text-[1.8rem] font-medium dark:text-zinc-300'>R$ {c.price.toFixed(2)}</span>
                                 <button onClick={()=> moveItemToFavorites(c.id)} className='dark:bg-zinc-100 dark:text-zinc-900 text-[1.4rem] font-medium p-[1rem] block mt-[.8rem] rounded-[.6rem]'>Mover para o carrinho</button>
                         </div>
-                        <button className='mb-auto ml-auto text-red-400'>
+                        <button className='absolute top-0 right-0 text-red-400'>
                             <Trash2 />
                         </button>
                     </div>)}
-                </div>
-                <div>
-                    <h1 className='text-[3rem] font-semibold leading-none'>Resumo do pedido</h1>
-                    <div className='flex items-center justify-between text-[1.6rem] max-w-[90%] mt-[.8rem]'>
-                        <p>Total de itens</p>
-                        <p>{totalItens}</p>
-                    </div>
-                    <div className='flex items-center justify-between text-[1.6rem] max-w-[90%] mt-[.8rem]'>
-                        <p>Desconto no pix</p>
-                        <p>R$ {discount.toFixed(2)}</p>
-                    </div>
-                    <div className='flex items-center justify-between text-[2rem] max-w-[90%] mt-[.8rem] font-semibold'>
-                        <p>Total</p>
-                        <p>R$ {totalValue.toFixed(2)}</p>
-                    </div>
                 </div>
             </div>
             <Message message={message} setMessage={setMessage}/>
