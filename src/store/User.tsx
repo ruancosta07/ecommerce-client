7d8bfc60cbe7b23c8da671431c73d36eefbadfa6
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-25 13:56:33 -0300

    feat: componente que adiciona o modo claro/escuro

--- src/store/User.tsx
+++ src/store/User.tsx
@@ -2,33 +2,53 @@
 
 import { Product } from "@/types/Product";
 import { User } from "@/types/User";
+import { useTheme } from "next-themes";
+import { ReactNode, useEffect, useState } from "react";
 import { create } from "zustand";
-
+import { ThemeProvider as NextThemeProvider } from "next-themes";
 
 interface UserContextType {
     user: User | null;
-    setUser: (value:User | null)=> void;
+    setUser: (value: User | null) => void;
     signed: boolean;
-    setSigned: (value:boolean)=> void
+    setSigned: (value: boolean) => void
     cart: Product[] | [],
-    setCart: (value: Product | [] )=> void
+    setCart: (value: Product | []) => void
     favorites: Product[] | [],
-    setFavorites: (value:Product | [])=> void
-    lastRoute:string | null;
-    setLastRoute: (value:string | null)=> void
-    theme?: string | undefined,
-    setTheme?: (value:string | undefined)=> void
+    setFavorites: (value: Product | []) => void
+    lastRoute: string | null;
+    setLastRoute: (value: string | null) => void
+    theme: string,
+    setTheme: (value: string) => void
 }
 
-export const useUser = create<UserContextType>((set )=> ({
-    cart:[],
-    setCart: (value)=> set({cart:value}),
-    favorites:[],
-    setFavorites: (value)=> set({favorites:value}),
+export const useUser = create<UserContextType>((set) => ({
+    cart: [],
+    setCart: (value) => set({ cart: value }),
+    favorites: [],
+    setFavorites: (value) => set({ favorites: value }),
     lastRoute: "",
-    setLastRoute: (value)=> set({lastRoute:value}),
-    signed:false,
-    setSigned: (value)=> set({signed:value}),
+    setLastRoute: (value) => set({ lastRoute: value }),
+    signed: false,
+    setSigned: (value) => set({ signed: value }),
     user: null,
-    setUser: (value)=> set({user:value})
+    setUser: (value) => set({ user: value }),
+    theme: "",
+    setTheme: (value) => set({ theme: value })
 }))
+
+
+export const ThemeProvider = ({children}: {children:ReactNode}) => {
+    const [mounted, setMounted] = useState(false)
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+
+    if(!mounted){
+        return <></>
+    }
+    return <NextThemeProvider attribute={"class"} defaultTheme={"system"}>
+        {children}
+    </NextThemeProvider>
+}
