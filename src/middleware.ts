167441a4c27795b12a598c719a0b8ad05c531fff
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-03-03 17:14:21 -0300

    refactor: utilização do middleware na rota de compra concluída

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,22 @@
 import { NextResponse } from "next/server";
-import type { NextRequest, MiddlewareConfig} from "next/server";
+import type { NextRequest, MiddlewareConfig } from "next/server";
 import signedMiddleware from "./middlewares/signed";
 import checkoutSessionExpired from "./middlewares/checkoutSessionExpired";
 import categoriesMiddleware from "./middlewares/categories";
 
-export function middleware(request:NextRequest){
-    if(request.nextUrl.pathname.startsWith("/usuario")){
+export function middleware(request: NextRequest) {
+    if (request.nextUrl.pathname.startsWith("/usuario") || request.nextUrl.pathname.startsWith("/complete")) {
         return signedMiddleware(request)
     }
-    if(request.nextUrl.pathname.startsWith("/complete")){
+    if (request.nextUrl.pathname.startsWith("/complete")) {
         return checkoutSessionExpired(request)
     }
-    if(request.nextUrl.pathname.startsWith("/categorias")){
+    if (request.nextUrl.pathname.startsWith("/categorias")) {
         return categoriesMiddleware(request)
     }
     return NextResponse.next()
 }
 
-export const config:MiddlewareConfig =  {
-    matcher: ["/usuario/:path*", "/complete", "/categorias"],
+export const config: MiddlewareConfig = {
+    matcher: ["/usuario/:path*", "/complete/:path*", "/categorias"],
 }
