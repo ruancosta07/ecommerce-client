382537d3c447e15337404ce0c471404eefae6cf6
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-04-21 14:39:42 -0300

    refactor: desativação da autenticação em dois fatores para usuários com provider

--- src/app/usuario/conta/seguranca/_components/Security.tsx
+++ src/app/usuario/conta/seguranca/_components/Security.tsx
@@ -9,11 +9,11 @@ const Security = () => {
   const { user, setUser } = useUser()
   const { checked, setChecked } = useSwitch()
 
-  useEffect(()=> {
+  useEffect(() => {
     setChecked(user?.twoStepsAuth as boolean)
-  },[setChecked, user])
+  }, [setChecked, user])
 
-  async function switchTwoStepsAuth(checked:boolean){
+  async function switchTwoStepsAuth(checked: boolean) {
     try {
       const response = await fetch(`${process.env.NEXT_PUBLIC_API_PROD_URL}/users/${user?.id}/`, {
         method: "PUT",
@@ -21,7 +21,7 @@ const Security = () => {
           "Authorization": `Bearer ${Cookies.get("authTokenUser")}`,
           "Content-Type": "application/json"
         },
-        body: JSON.stringify({user:{...user, twoStepsAuth:checked}}),
+        body: JSON.stringify({ user: { ...user, twoStepsAuth: checked } }),
       })
       const data = await response.json() as User
 
@@ -35,15 +35,17 @@ const Security = () => {
     <section>
       <div>
         <div className="flex gap-[1rem]">
-          <SwitchPrimitive.Root aria-checked={checked} checked={checked} onCheckedChange={(e) => {
-            switchTwoStepsAuth(e)
-          }} className={'bg-zinc-800 w-[5rem] h-[2.5rem] relative p-[.2rem] rounded-full aria-checked:bg-zinc-100 duration-200'}>
+          <SwitchPrimitive.Root aria-checked={checked} checked={checked} disabled={!!user?.provider} onCheckedChange={(e) => {
+            if (!user?.provider) {
+              switchTwoStepsAuth(e)
+            }
+          }} className={'bg-zinc-800 w-[5rem] h-[2.5rem] relative p-[.2rem] rounded-full aria-checked:bg-zinc-100 duration-200 disabled:opacity-70 disabled:cursor-not-allowed'}>
             <SwitchPrimitive.Thumb className='bg-zinc-100 size-[2.1rem] block rounded-full duration-200 relative data-[state=checked]:translate-x-[2.5rem] translate-x-[.2rem] data-[state=checked]:bg-zinc-900' />
           </SwitchPrimitive.Root>
-        <div>
-        <h1 className='text-[2rem] font-semibold text-zinc-100'>Verificação em duas etapas</h1>
-        <p className='text-zinc-300 text-[1.4rem]'>Receba um email com um código para fazer login</p>
-        </div>
+          <div>
+            <h1 className='text-[2rem] font-semibold text-zinc-100'>Verificação em duas etapas</h1>
+            <p className='text-zinc-300 text-[1.4rem]'>Receba um email com um código para fazer login</p>
+          </div>
         </div>
       </div>
     </section>
